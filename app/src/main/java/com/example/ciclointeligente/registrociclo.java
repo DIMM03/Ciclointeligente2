ea58d2f9226c5dd9ce62b267703c384e470c29f7
DIMM03 <134746827+DIMM03@users.noreply.github.com>
2023-05-26 17:49:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ciclointeligente/registrociclo.java
@@ -0,0 +1,14 @@
+package com.example.ciclointeligente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrociclo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrociclo);
+    }
+}
